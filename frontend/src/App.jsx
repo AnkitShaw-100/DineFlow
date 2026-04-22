5c5f1720e2549fd22fb11e33c2eec1fd8cf32716
AnkitShaw-100 <ankitshaw6933@gmail.com>
2026-04-22 07:54:54 +0530

    frontend setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl font-extrabold text-center m-6'>
+      Quick Bill App 
+    </div>
+  )
+}
+
+export default App
